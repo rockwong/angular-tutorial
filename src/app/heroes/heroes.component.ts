7cab72954126293b0971189d6e96906e6f5bd3fc
rockwong <rockwong.cn@gmail.com>
2019-01-07 22:54:03 +0800

    create hero componet

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
   constructor() {}
-  hero = 'Windstrom';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
   ngOnInit() {}
 }
