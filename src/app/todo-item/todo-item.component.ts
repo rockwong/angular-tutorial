f5e79734d69c84ecb9c124c7da68eb6d4838d537
rockwong <rockwong.cn@gmail.com>
2019-02-12 11:39:13 +0800

    增加样式

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.scss']
+  styleUrls: ['./todo-item.component.scss'],
 })
 export class TodoItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {}
+  @Input() title = 'Untitled';
+  @Input() completed: string;
+  @Input() id: string;
+  @Output() toggle: EventEmitter<any> = new EventEmitter();
+  @Output() delete: EventEmitter<any> = new EventEmitter();
+  ngOnInit() {}
+  onToggle() {
+    this.toggle.emit(!this.completed);
   }
-
+  Output() {
+    this.delete.emit(this.id);
+  }
 }
