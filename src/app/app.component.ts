f138557ebf76aeb50c8e4d02c9479c4e813fbcdd
rockwong <rockwong.cn@gmail.com>
2019-02-12 17:33:37 +0800

    事件绑定完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,13 @@ export class AppComponent implements OnInit {
   constructor(private listService: ListService, private cd: ChangeDetectorRef) {}
   title = 'angular-todo';
   newTitle = '';
+  filterType = -1;
   list: ITodoItem[];
+  get finalList() {
+    if (this.filterType === 0) return this.list.filter(item => !item.completed);
+    else if (this.filterType === 1) return this.list.filter(item => item.completed);
+    else return this.list;
+  }
   ngOnInit(): void {
     this.onFetch();
   }
@@ -26,5 +32,20 @@ export class AppComponent implements OnInit {
     this.listService.add(title).subscribe(() => {
       this.onFetch();
     });
+  }
+  onDelete(id) {
+    console.log('onDelete id===', id);
+    this.listService.deleteByIds({ ids: [id] }).subscribe(() => {
+      this.onFetch();
+    });
+  }
+  onToggle(id) {
+    this.listService.toggleById(id).subscribe(() => {
+      this.onFetch();
+    });
+  }
+  onTypeChange(type) {
+    console.log('onTypeChange type==', type);
+    this.filterType = type;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoItemComponent],
+  declarations: [AppComponent, TodoItemComponent, TodoFooterComponent],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
